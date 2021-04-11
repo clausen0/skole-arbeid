c3032bce807735f9842616e3678b7db7fe14f4c8
clausen0 <81365003+clausen0@users.noreply.github.com>
2021-04-11 11:42:44 +0200

    Add files via upload

--- laberynt.py
+++ laberynt.py
@@ -18,24 +18,60 @@ currentPath = "hoved kode/"
 BLACK = (25, 25, 200)
 WHITE = (254, 254, 254)
 
-main = True
+# main = True
+vel = 2
 
 ...
 # objekter
 ...
 
 
-class Player(pygame.sprite.Sprite):  # spiller klasse
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.images = []
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vel = vel
 
-        #img = pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-        img = pygame.image.load(os.path.join(
-            currentPath + 'images', 'hero.png')).convert()
-        self.images.append(img)
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
+
+class Player:  # (pygame.sprite.Sprite):  # spiller klasse
+    def __init__(self, pos: Vector2):
+        self.heading = 0
+        self.char = [pygame.image.load(os.path.join(currentPath + 'sprites', 'hero_idle.png')), pygame.image.load(
+            os.path.join(currentPath + 'sprites', 'hero_left.png')), pygame.image.load(os.path.join(currentPath + 'sprites', 'hero_right.png')), pygame.image.load(os.path.join(currentPath + 'sprites', 'hero_up.png')),
+            pygame.image.load(os.path.join(currentPath + 'sprites', 'hero_down.png'))]
+        self.x = pos.x
+        self.y = pos.y
+        self.spawn = pos
+        screen.blit(self.char[self.heading], (self.spawn.x, self.spawn.y))
+
+    def draw(self):
+        screen.blit(self.char[self.heading], (self.x, self.y))
+ n
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_s]:
+            self.y += vel
+            self.heading = 4
+        if keys[pygame.K_w]:
+            self.y -= vel
+            self.heading = 3
+        if keys[pygame.K_a]:
+            self.x -= vel
+            self.heading = 1
+        if keys[pygame.K_d]:
+            self.x += vel
+            self.heading = 2
+
+    def rect(self):
+        return self.image.get_rect()
+
+
+def draw_window():
+    screen.blit(backdrop, (0, 0))
+    player.update()
+    player.draw()
+
+    pygame.display.update()
 
 
 ...
@@ -48,39 +84,58 @@ pygame.init()
 
 screen = pygame.display.set_mode((1200, 650))
 
-player = Player
-# player.rect = 0
-#player.rect = 0
-player_list = pygame.sprite.Group()
-player_list.add(player)
+  
+# player_list = pygame.sprite.Group()
+# player_list.add(player)
 
 # under er hvordan bakrunnen til spillet skal se ut. Denne delen av koden skal fikse slik at man greier å hente frem informasjon
 # fra img filer som er lagret i mappen som heter images
 
 world = pygame.display.set_mode([worldx, worldy])
-backdrop = pygame.image.load_extended(currentPath + "/images/stage.png")
+# pygame.image.load_extended(currentPath + "/images/stage.png")
+backdrop = pygame.image.load_extended(
+    os.path.join(currentPath + 'sprites', 'background.png'))
 # backdropbox = world.get_reackt
 
 ...
 # main.loop
 ...
 
-while main:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            try:
-                sys.exit()
-            finally:
-                main = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == ord('q'):
+
+def main():
+
+    run = True
+
+    while run:
+        print('running')
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print('quitting')
                 pygame.quit()
-            try:
-                sys.exit()
-            finally:
-                main = False
+                try:
+                    sys.exit()
+                finally:
+                    run = False
+            # if event.type == pygame.KEYDOWN:
+            #    print('quitting')
+            #    if event.key == ord('q'):
+            #        pygame.quit()
+            #    try:
+            #        sys.exit()
+            #    finally:
+            #        main = False
+
+        draw_window()
+
     # world.blit(backdrop. backdropbox)  # refresher bakrunen
     # pygame.display.flip()  # refresher alt på skjermen
-    player_list.draw(world)
-    clock.tick(fps)
+
+    # player_list.draw(world)
+
+
+if __name__ == '__main__':
+    pos = Vector2(50, 80)
+    player = Player(pos)
+
+    main()
