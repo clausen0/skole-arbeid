b14931abbdeddb20e56e7aa6fc52cd7c927afa9e
clausen0 <81365003+clausen0@users.noreply.github.com>
2021-09-22 22:30:25 +0200

    Add files via upload

---
+++ oblig3/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="../ForsideStyle.css">
+</head>
+<body>
+    <ul>
+        <li><a class="active" href="../index.php">Forside</a></li>
+    </ul>
+    <p><a href="5/index.php">Oppgave 5</p>
+    <p><a href="6/index.php">Oppgave 6</p>
+</body>
+</html>
